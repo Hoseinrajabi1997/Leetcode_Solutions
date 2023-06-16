edb4245636f79799634aa5a0689b02c13702e589
unknown <hoseinrajabi1997@gmail.com>
2023-06-15 21:35:55 -0500

    search in sorted rotated

---
+++ Binary_Search/33. Search in Rotated Sorted Array - MEDIUM.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l<=r:
+            mid = l + (r-l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < nums[r]: #lright sorted
+                if target < nums[mid] or target > nums[r]:
+                    r = mid -1
+                else:
+                    l = mid + 1
+            else: # left sorted
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
